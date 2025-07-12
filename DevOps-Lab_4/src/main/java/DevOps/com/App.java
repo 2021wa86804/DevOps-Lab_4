9b73d4652d7b6daf1022b49643ee0f21df1974a5
Iftekhar Shahil <2021wa86804@wilp.bits-pilani.ac.in>
2025-07-12 11:17:23 +0530

    Initial Maven Project

---
+++ DevOps-Lab_4/src/main/java/DevOps/com/App.java
@@ -0,0 +1,13 @@
+package DevOps.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
